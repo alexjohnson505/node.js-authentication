36114c75f108cb0ecbee7f3d3d64c002bc0520c3
Alex Johnson <alex@tablelist.com>
2015-03-24 18:21:47 -0400

    Improve UI

--- public/js/app.js
+++ public/js/app.js
@@ -26,22 +26,18 @@ app.config(function($routeProvider, $httpProvider) {
       });
 });
 
-var checkLoggedin = function($q, $timeout, $http, $location, $rootScope)
-{
+var checkLoggedin = function($q, $timeout, $http, $location, $rootScope){
     var deferred = $q.defer();
 
-    $http.get('/loggedin').success(function(user)
-    {
+    $http.get('/loggedin').success(function(user){
         $rootScope.errorMessage = null;
         // User is Authenticated
-        if (user !== '0')
-        {
+        if (user !== '0'){
             $rootScope.currentUser = user;
             deferred.resolve();
         }
         // User is Not Authenticated
-        else
-        {
+        else {
             $rootScope.errorMessage = 'You need to log in.';
             deferred.reject();
             $location.url('/login');
@@ -50,13 +46,3 @@ var checkLoggedin = function($q, $timeout, $http, $location, $rootScope)
     
     return deferred.promise;
 };
-
-app.controller("NavCtrl", function($scope, $http, $location, $rootScope){
-    $scope.logout = function(){
-        $http.post("/logout")
-        .success(function(){
-            $rootScope.currentUser = null;
-            $location.url("/home");
-        });
-    };
-});
--- public/views/register/register.js
+++ public/views/register/register.js
@@ -1,17 +1,13 @@
 app.controller("RegisterCtrl", function($scope, $http, $location, $rootScope){
     $scope.register = function(user){
-        console.log(user);
-        if(user.password != user.password2 || !user.password || !user.password2)
-        {
+
+        if(user.password != user.password2 || !user.password || !user.password2){
             $rootScope.message = "Your passwords don't match";
-        }
-        else
-        {
+        } else {
             $http.post("/register", user)
             .success(function(response){
-                console.log(response);
-                if(response != null)
-                {
+            
+                if(response != null){
                     $rootScope.currentUser = response;
                     $location.url("/profile");
                 }
